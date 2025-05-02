699617980450e2b12a859134913c94ce1d2b73d3
gabriel <gabriel.otaciliodev@gmail.com>
2025-05-02 18:16:47 -0300

    adicionando classe Turma

--- src/Entidades/Aluno.java
+++ src/Entidades/Aluno.java
@@ -1,7 +1,54 @@
 package Entidades;
 
 public class Aluno {
-	
+	 private long matricula;
+	 private String nome;
+	 private String curso;
+	 private boolean especial;
+	 
+	 
+	public Aluno(long matricula, String nome, String curso, boolean especial) {
+		super();
+		this.matricula = matricula;
+		this.nome = nome;
+		this.curso = curso;
+		this.especial = especial;
+	}
+
+
+	public long getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(long matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public boolean isEspecial() {
+		return especial;
+	}
+
+	public void setEspecial(boolean especial) {
+		this.especial = especial;
+	}
+	 
+	 
 	
 		
 }
