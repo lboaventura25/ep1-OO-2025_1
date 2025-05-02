20e7122a671558ab94d8bacee80ef00ba764a765
gabriel <gabriel.otaciliodev@gmail.com>
2025-05-02 15:10:21 -0300

    adicionando pacote Modo-disciplina , com suas classes.

---
+++ src/Modo_Disciplina/Modo_Disciplinas.java
@@ -0,0 +1,24 @@
+package Modo_Disciplina;
+
+public class Modo_Disciplinas {
+	
+	public void cadastrasDisciplinas() {
+		
+		
+	}
+	
+	public void cadastrarturmas() {
+		
+	}
+	
+	public void listarDisciplinas() {
+		
+	}
+	
+	public void listarTurmas() {
+		// por disciplinas????
+	}
+	
+	
+
+}
