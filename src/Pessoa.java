5522c1cb8f769d19a6be626c0366dfbb22732ec2
Administrator <heitorbsbdf@gmail.com>
2025-05-15 09:13:15 -0300

    Initial commit

---
+++ src/Pessoa.java
@@ -0,0 +1,27 @@
+public abstract class Pessoa {
+    private String nome;
+    private String matricula;
+
+    public Pessoa(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String toString() {
+        return "Nome: " + nome + "\nMatricula: " + matricula;
+    }
+}
