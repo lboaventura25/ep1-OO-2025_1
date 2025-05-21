122697f16c63e9b450185d6af8df7f8e056e4302
Galdino <pedro.wollmann@gmail.com>
2025-05-21 19:10:12 -0300

    foram adicionados os atributos nomeDisciplina, nomeProfessor, listaMatriculados

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -1,35 +1,60 @@
+import java.util.ArrayList;
+
 public class Disciplina {
-    //private String codigo;
-    //private Integer capacidadeMaxima;
-    private String nome;
+    private String codigo;
+    private Integer capacidadeMaxima;
+    private String nomeDisciplina;
+    private String nomeProfessor;
+    private ArrayList<Aluno> listaMatriculados;
 
 
 
-    public Disciplina(String nome){
-        this.nome = nome;
+    public Disciplina(String nomeDisciplina, String codigo, Integer capacidadeMaxima, String nomeProfessor, ArrayList<Aluno> listaMatriculados){
+        this.nomeDisciplina = nomeDisciplina;
+        this.codigo = codigo;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.nomeProfessor = nomeProfessor;
+        this.listaMatriculados = listaMatriculados;
+
+
     }
-//    public void setCodigo(String codigo){
-//        this.codigo = codigo;
-//    }
-//
-//    public String getCodigo(){
-//        return this.codigo;
-//    }
-
-//    public Integer getCapacidadeMaxima(){
-//        return this.capacidadeMaxima;
-//    }
-//
-//    public void setCapacidadeMaxima(Integer capacidade){
-//        this.capacidadeMaxima = capacidade;
-//    }
-
-    public String getNome(){
-        return this.nome;
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
     }
 
-    public void setNome(String nome){
-        this.nome = nome;
+    public String getCodigo(){
+        return this.codigo;
+    }
+
+    public Integer getCapacidadeMaxima(){
+        return this.capacidadeMaxima;
+    }
+
+    public void setCapacidadeMaxima(Integer capacidade){
+        this.capacidadeMaxima = capacidade;
+    }
+
+    public String getNomeDisciplina(){
+        return this.nomeDisciplina;
+    }
+
+    public void setNomeDisciplina(String nome){
+        this.nomeDisciplina = nome;
+    }
+
+    public ArrayList<Aluno> getListaMatriculados() {
+        return this.listaMatriculados;
+    }
+
+    public void setListaMatriculados(ArrayList<Aluno> listaMatriculados){
+        this.listaMatriculados = listaMatriculados;
+    }
+
+    public String getNomeProfessor(){
+        return this.nomeProfessor;
+    }
+    public void setNomeProfessor(String nomeProfessor){
+        this.nomeProfessor = nomeProfessor;
     }
 }
 
