c63cff43c67da9c04453ab464e6f4ff9439dd5ea
Galdino <pedro.wollmann@gmail.com>
2025-05-25 17:03:31 -0300

    logica do modo aluno transferida para classe Aluno.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         ArrayList<Disciplina> listaDisciplinas = new ArrayList<>();
         while (true){
 
             System.out.println("""
-                    Bem vindo ao sigaa
+                    ANTES DE CADASTRAR UM ALUNO, CADASTRE UMA DISCIPLINA!
                     Qual modo deseja escolher?
                     1 - Modo aluno
                     2 - Modo Disciplina
@@ -16,36 +16,7 @@ public class Main {
                     """);
             Integer opcao = leitor.nextInt();
             if(opcao == 1){
-                System.out.println("""
-                        Voce deseja:
-                        1- Cadastrar um aluno
-                        2- Ver alunos cadastrados
-                        """);
-                //Codigo quebra quando nenhuma opcao eh selecionada CORRIGIR ANTES DE MANDAR
-                Integer modoAluno = leitor.nextInt();
-                leitor.useDelimiter(System.lineSeparator());
-                if (modoAluno == 1){
-                    //verificar se o nome ja existe no banco de dados
-                    System.out.println("Digite o nome do aluno: ");
-                    String nomeAluno = leitor.next();
-                    //verificar se a matricula ja existe no banco de dados
-                    System.out.println("Digite a matricula do aluno:");
-                    String matriculaAluno = leitor.next();
-
-                    //verificar se esta sendo digitado um curso mesmo
-                    System.out.println("Digite o curso de graduacao do aluno:");
-                    String cursoAluno = leitor.next();
-
-                    Aluno aluno1 = new Aluno(nomeAluno, matriculaAluno, cursoAluno );
-                    Aluno.salvarAluno(aluno1, "teste.csv");
-                }
-                if (modoAluno == 2){
-                    ArrayList<Aluno> alunos = Aluno.listarAlunos("teste.csv");
-                    for(Aluno i : alunos){
-                        i.formatado();
-                    }
-                }
-
+                Aluno.menuAluno(listaDisciplinas);
             }
 
             else if (opcao == 2){
@@ -58,7 +29,7 @@ public class Main {
                 int modoDisciplina = leitor.nextInt();
                 //temporario, nao mostra os alunos matriculados ainda
                 //ver se ele cria um arquivo novo ou usa o existente
-                ArrayList<Aluno> listaMatriculados = Aluno.listarAlunos("teste.csv");
+                ArrayList<Aluno> listaMatriculados = new ArrayList<>();
                 
                 //teste remover depois
                 if(modoDisciplina == 1){
