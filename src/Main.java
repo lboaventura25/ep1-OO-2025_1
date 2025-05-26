13acb149285ffba40f689a8af06ed85c16abdbd8
Galdino <pedro.wollmann@gmail.com>
2025-05-26 17:07:14 -0300

    Logica do modo disciplina foi transferida para outro arquivo

--- src/Main.java
+++ src/Main.java
@@ -19,48 +19,9 @@ public class Main {
                 Aluno.menuAluno(listaDisciplinas);
             }
 
-            else if (opcao == 2){
-                //Adicionar a opcao de voltar para o menu anterior
-                System.out.println("""
-                        Voce deseja:
-                        1- Cadastrar uma disciplina
-                        2- Ver disciplinas cadastradas
-                        """);
-                int modoDisciplina = leitor.nextInt();
-                //temporario, nao mostra os alunos matriculados ainda
-                //ver se ele cria um arquivo novo ou usa o existente
-                ArrayList<Aluno> listaMatriculados = new ArrayList<>();
-                
-                //teste remover depois
-                if(modoDisciplina == 1){
-
-                    System.out.println("Digite o nome da disciplina:");
-                    // ver se o codigo quebra caso o nome seja maior que uma palavra
-                    String nomeDisciplina = leitor.next();
-
-                    System.out.println("Digite o codigo da disciplina:");
-                    String codigoDisciplina = leitor.next();
-
-                    System.out.println("digite o professor discente:");
-                    String nomeProfessor = leitor.next();
-
-                    System.out.println("digite a quantidade de vagas disponiveis na diciplina");
-                    Integer vagasDisponiveis = leitor.nextInt();
-
-
-                    Disciplina disciplina = new Disciplina(nomeDisciplina, codigoDisciplina,vagasDisponiveis,nomeProfessor,listaMatriculados);
-                    listaDisciplinas.add(disciplina);
-                    // nao esta completo ainda, so um teste
-                    //ver se aceita nomes com mais de uma palavra
-                    Disciplina.arquivarDisciplina(nomeDisciplina, codigoDisciplina, nomeProfessor, vagasDisponiveis );
-
-                }
-                else if(modoDisciplina == 2){
-                    //temporario, nao mostra as diciplinas cadastradas ainda
-                    Disciplina.listarDisciplinas(listaDisciplinas);
-                }
+            else if (opcao == 2) {
+            Disciplina.menuDisciplina(listaDisciplinas);
             }
-
             else if(opcao == 3){
                 break;
             }
