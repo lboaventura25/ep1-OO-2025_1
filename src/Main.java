158f8fc7f18d8f7776b620688e86056d39b4462c
Galdino <pedro.wollmann@gmail.com>
2025-05-26 20:52:03 -0300

    agora inicializa as disciplinas ao iniciar o programa

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         ArrayList<Disciplina> listaDisciplinas = new ArrayList<>();
+        Disciplina.iniciarDisciplina(listaDisciplinas);
         while (true){
 
             System.out.println("""
