6c0f6ca1cddbb6fa7e3a022d70f7f39c68612e34
Galdino <pedro.wollmann@gmail.com>
2025-05-26 23:19:01 -0300

    Menu disciplina retirado

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -175,44 +175,6 @@ public class Disciplina {
         return aluno;
     }
 
-    public static void menuDisciplina(ArrayList<Disciplina> listaDisciplinas){
-        Scanner leitor = new Scanner(System.in);
-        //Adicionar a opcao de voltar para o menu anterior
-        System.out.println("""
-                        Voce deseja:
-                        1- Cadastrar uma disciplina
-                        2- Ver disciplinas cadastradas
-                        """);
-        int modoDisciplina = leitor.nextInt();
-        //temporario, nao mostra os alunos matriculados ainda
-        //ver se ele cria um arquivo novo ou usa o existente
-        ArrayList<Aluno> listaMatriculados = new ArrayList<>();
 
-        //teste remover depois
-        if(modoDisciplina == 1){
-
-            System.out.println("Digite o nome da disciplina:");
-            // ver se o codigo quebra caso o nome seja maior que uma palavra
-            String nomeDisciplina = leitor.next();
-
-            System.out.println("Digite o codigo da disciplina:");
-            String codigoDisciplina = leitor.next();
-
-            System.out.println("digite o professor discente:");
-            String nomeProfessor = leitor.next();
-
-            System.out.println("digite a quantidade de vagas disponiveis na diciplina");
-            Integer vagasDisponiveis = leitor.nextInt();
-
-
-            Disciplina disciplina = new Disciplina(nomeDisciplina, codigoDisciplina,vagasDisponiveis,nomeProfessor,listaMatriculados);
-            listaDisciplinas.add(disciplina);
-            // nao esta completo ainda, so um teste
-            //ver se aceita nomes com mais de uma palavra
-        }
-        else if(modoDisciplina == 2){
-            Disciplina.listarDisciplinas(listaDisciplinas);
-        }
-    }
 }
 
