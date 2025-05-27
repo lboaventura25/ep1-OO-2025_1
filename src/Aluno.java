8f90c8965742b95556e988c559d0caa110818f6b
Galdino <pedro.wollmann@gmail.com>
2025-05-26 23:01:09 -0300

    menuAluno trasferido para uma nova classe

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Aluno {
     private String nome;
@@ -90,45 +89,5 @@ public class Aluno {
         return alunos;
     }
 //fazer uma view dps
-    public static void menuAluno(ArrayList<Disciplina> listaDisciplinas){
-        Scanner leitor = new Scanner(System.in);
-            System.out.println("""
-                        Voce deseja:
-                        1- Cadastrar um aluno
-                        2- Ver alunos cadastrados
-                        """);
-            //Codigo quebra quando nenhuma opcao eh selecionada CORRIGIR ANTES DE MANDAR
-            Integer modoAluno = leitor.nextInt();
-            leitor.useDelimiter(System.lineSeparator());
-            if (modoAluno == 1){
-                //verificar se o nome ja existe no banco de dados
-                System.out.println("Digite o nome do aluno: ");
-                String nomeAluno = leitor.next();
-                //verificar se a matricula ja existe no banco de dados
-                System.out.println("Digite a matricula do aluno:");
-                String matriculaAluno = leitor.next();
 
-                //verificar se esta sendo digitado um curso mesmo
-                System.out.println("Digite o curso de graduacao do aluno:");
-                String cursoAluno = leitor.next();
-
-                Aluno aluno1 = new Aluno(nomeAluno, matriculaAluno, cursoAluno );
-                Aluno.salvarAluno(aluno1, "teste.csv");
-                //adicionar sistema de contador para as disciplinas terem numeros
-                int cont = 1;
-                for(Disciplina i : listaDisciplinas){
-                    i.formatado(cont);
-                }
-                System.out.println("Selecione a disciplina que quer matricula-lo:");
-                int escolhaDisciplina = leitor.nextInt() - 1;
-                listaDisciplinas.get(escolhaDisciplina).matricularAluno(aluno1);
-            }
-            if (modoAluno == 2){
-                ArrayList<Aluno> alunos = Aluno.listarAlunos("teste.csv");
-                for(Aluno i : alunos){
-                    i.formatado();
-                }
-            }
-
-        }
     }
